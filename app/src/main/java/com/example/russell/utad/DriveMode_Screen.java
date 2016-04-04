3005612553d9119ecb135f930b0c8c95eeacfa87
rufflezberg <erickson.russell.j@gmail.com>
2016-04-03 21:57:45 -0500

    Home screen designed with navigation to drive mode screen.

---
+++ app/src/main/java/com/example/russell/utad/DriveMode_Screen.java
@@ -0,0 +1,20 @@
+package com.example.russell.utad;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Russell on 4/3/2016.
+ */
+public class DriveMode_Screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drivemode_screen);
+
+    }
+}
