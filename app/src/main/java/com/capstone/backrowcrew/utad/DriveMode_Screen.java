afdb48224c5921d53622fc72a184273f1c59ff08
dHudykaE <david.t.hudyka@net.elmhurst.edu>
2016-05-04 17:16:50 -0500

    Audio Changes
    OnCreate of DriveMode silences notification volumes. Pressing the Exit
    Drive Mode button returns to the previous volume settings.

--- app/src/main/java/com/capstone/backrowcrew/utad/DriveMode_Screen.java
+++ app/src/main/java/com/capstone/backrowcrew/utad/DriveMode_Screen.java
@@ -1,7 +1,9 @@
 package com.capstone.backrowcrew.utad;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +18,16 @@ public class DriveMode_Screen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drivemode_screen);
 
+        AudioManager audioManager= (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+
         //navigation to home screen when save button is pressed
         Button exitDriveModeButton=(Button)findViewById(R.id.exitDriveMode);
         exitDriveModeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent toHomeScreen = new Intent(view.getContext(), Home_Screen.class);
+                AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 startActivity(toHomeScreen);
             }
         });
